921470cba6863860814cb3af9f3221380ff2c3e0
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2025-01-31 08:49:51 +0700

    chore: Initialize iOS project with basic setup

---
+++ HTTubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTubeApp
+//
+//  Created by Thạnh Dương Hoàng on 31/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
