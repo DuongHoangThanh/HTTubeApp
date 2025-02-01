ea900f291cab3baf1dddd41f738f0fbee4dbdf2c
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2025-02-01 18:28:27 +0700

    WIP: Temporary commit before rebase

---
+++ HTTubeApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  HTTubeApp
+//
+//  Created by Thạnh Dương Hoàng on 31/1/25.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = ""
+    static var PLAYLIST_ID = "RDq-1FuU37zvA"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(PLAYLIST_ID)&key=\(API_KEY)"
+}
+    
